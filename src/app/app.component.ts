c6034a7089d89763827c7c5e9c7783b05442bfe5
Surya Teja K <shanmukhateja@gmail.com>
2021-04-29 10:54:51 +0530

    feat: Add angular-datatables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor() {}
+  dtOptions: DataTables.Settings = {};
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers'
+    };
   }
 }
